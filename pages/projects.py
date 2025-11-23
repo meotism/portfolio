d13eb420ad0f0e8dbf80bd29c124611783ba57c7
meotism <57476209+meotism@users.noreply.github.com>
2025-11-23 07:27:37 +0000

    add information 2 newest pet project

--- pages/projects.py
+++ pages/projects.py
@@ -16,8 +16,24 @@ with body:
         st.subheader(header, divider='grey')
         st.write(content)
 
-    # Chatbot’s Phi  ----------------------------------------------------------------
+    # cap cuu cuu ho
     project_component(Projects[1][0], Projects[1][1])
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.link_button("Go to :blue[Source Code]", "https://github.com/meotism/Cap-cuu-cuu-ho")
+    with col2:
+        st.link_button("Go to :blue[Website]", "https://meotism.pythonanywhere.com/")
+
+    # Flashcard English
+    project_component(Projects[2][0], Projects[2][1])
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.link_button("Go to :blue[Source Code]", "https://github.com/meotism/flashcard")
+    with col2:
+        st.link_button("Go to :blue[Website]", "https://flashcard.pythonanywhere.com/")
+
+    # Chatbot’s Phi  ----------------------------------------------------------------
+    project_component(Projects[3][0], Projects[3][1])
     col1, col2 = st.columns([1, 1])
     with col1:
         st.link_button("Go to :blue[Source Code]", "https://github.com/meotism/chatbot/blob/main/streamlit_app.py")
@@ -25,20 +41,20 @@ with body:
         st.link_button("Go to :blue[Website]","https://phi-chatbot.streamlit.app")
 
     # Review University ----------------------------------------------------------------
-    project_component(Projects[2][0], Projects[2][1])
+    project_component(Projects[4][0], Projects[4][1])
     st.link_button("Go to :orange[Source Code]", "https://github.com/meotism/Quality_Assessment_Review/tree/master")
     components.iframe("https://docs.google.com/presentation/d/1a14wXlSTbYt6-oknulVRM6DZLjObwIFTfY9725mR3fk/edit?usp=sharing", width=800, height=600, scrolling=True)
 
     # Swiftlet’s Nest Anh Xuan --------------------------------------------------------------
-    project_component(Projects[3][0], Projects[3][1])
+    project_component(Projects[5][0], Projects[5][1])
     st.video("https://www.youtube.com/watch?v=UMMOqetDDLg")
 
     # Video Conference meeting -------------------------------------------------------------- 
-    project_component(Projects[4][0], Projects[4][1])
+    project_component(Projects[6][0], Projects[6][1])
     st.link_button("Go to :red[Source Code]", "https://github.com/meotism/jitsi-meet")
 
     # My portfolio -----------------------------------------------------------
-    project_component(Projects[5][0], Projects[5][1])
+    project_component(Projects[7][0], Projects[7][1])
     col1, col2 = st.columns([1, 1])
     with col1:
         st.link_button("Go to :blue[Source Code]", "https://github.com/meotism/portfolio/tree/main")
